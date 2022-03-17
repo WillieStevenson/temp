bb2c18315ab4843b1b8e7db7bf8d3505e20829e5
Willie S <willie.o.stevenson@gmail.com>
2022-03-17 14:38:53 -0400

    Add context comments regarding socket.inet_aton usage

--- app/views.py
+++ app/views.py
@@ -29,6 +29,7 @@ def index():
 		# examples such as '1' and '1.555' can be 
 		# as valid IPs. The former condition
 		# compensates for this.
+		# See https://docs.python.org/3/library/socket.html#socket.inet_aton
 		try:
 			if ip.count(".") == 3 and socket.inet_aton(ip):
 				messages.append({'status': "Success", 'message': "Valid IP"})
@@ -40,7 +41,6 @@ def index():
 
 				return render_template("index.html", messages=messages), 200
 			else:
-				# invalid ip, return 400
 				messages.append({'status': "Failure", 'message': "Please enter a valid IP."})
 				return render_template("index.html", messages=messages), 400
 		except:
