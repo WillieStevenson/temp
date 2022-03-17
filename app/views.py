9a2a69c9b9360371105009a84a3c6f4748c02949
Willie S <willie.o.stevenson@gmail.com>
2022-03-17 09:15:29 -0400

    Add flask imports and geoip2 and socket dependencies

--- app/views.py
+++ app/views.py
@@ -1,5 +1,20 @@
+from flask import render_template
 from app import app
 
-@app.route('/')
+from flask import Flask
+from flask import request
+from flask import url_for
+from flask import flash
+from flask import redirect
+
+import socket
+import geoip2.database
+
+app = Flask(__name__)
+from app import views
+
+
+@app.route('/', methods=('GET', 'POST'))
 def index():
-	return "Test."
+
+	return render_template("index.html")
