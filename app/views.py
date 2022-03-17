fc1e2770b88871049bbcd9faff26739cf41db24b
Willie S <willie.o.stevenson@gmail.com>
2022-03-17 09:36:45 -0400

    Add logic for landing page route that checks IP and returns lat/lon

--- app/views.py
+++ app/views.py
@@ -17,4 +17,36 @@ from app import views
 @app.route('/', methods=('GET', 'POST'))
 def index():
 
-	return render_template("index.html")
+	# This reader object should be reused across lookups as creation of it is
+	# expensive.
+	
+
+	messages = []
+
+	if request.method == "POST":
+		ip = request.form['ip']
+
+		
+		# according socket.inet_aton method documentation
+		# examples such as '1' and '1.555' can be 
+		# as valid IPs. The former condition
+		# compensates for this.
+		try:
+			if ip.count(".") == 3 and socket.inet_aton(ip):
+				messages.append({'status': "Success", 'message': "Valid IP"})
+
+				with geoip2.database.Reader('./app/static/GeoLite2-City.mmdb') as reader:
+				    response = reader.city(ip);
+
+				messages.append({'status': "Data", 'message': "Latitude: {}, Longitude: {} ".format(response.location.latitude, response.location.longitude)})
+
+				return render_template("index.html", messages=messages)
+			else:
+				# invalid ip, return 400
+				messages.append({'status': "Failure", 'message': "Please enter a valid IP."})
+				return render_template("index.html", messages=messages)
+		except:
+			messages.append({'status': "Failure", 'message': "Please enter a valid IP."})
+			return render_template("index.html", messages=messages)
+	else:
+		return render_template("index.html")
